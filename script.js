d0f1ade183f067b202dfe01c2e1ac58b57e080ac
Arnold W <arnoldwwong@gmail.com>
2023-09-06 16:06:52 +1000

    Initial commit; add DOMs

---
+++ script.js
@@ -0,0 +1,15 @@
+const cellBlocks = document.querySelectorAll('.cell');
+const displayText = document.querySelector('.display-text');
+const restartButton = document.querySelector('.restart-button');
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let cellChoices = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
